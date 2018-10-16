21fd276d8a30650e914b6f49b6ebee135a66ae5f
Stanislav Seletskiy <s.seletskiy@gmail.com>
2018-10-16 16:10:21 +0300

    remove tests

--- src/main/resources/js/pull-requests-labels.js
+++ src/main/resources/js/pull-requests-labels.js
@@ -1,17 +1,20 @@
 (function($) {
-    // Set up our namespace
-    window.Reconquest = window.Reconquest || {};
-    Reconquest.Labels = Reconquest.Labels || {};
+    alert(2);
+    //// Set up our namespace
+    //window.Reconquest = window.Reconquest || {};
+    //Reconquest.Labels = Reconquest.Labels || {};
 
     /**
      * The client-context-provider function takes in context and transforms
      * it to match the shape our template requires.
      */
-    function getLabelsForPR(context) {
-        console.log(context['pullRequest']);
-        return {}
-    }
+    //function init(context) {
+    //    console.log(context);
+    //    return {}
+    //}
 
-    /* Expose the client-context-provider function */
-    Reconquest.Labels.getLabelsForPR = getLabelsForPR;
+    //[> Expose the client-context-provider function <]
+    //Reconquest.Labels.Init = init;
 }(AJS.$));
+
+alert(3);
